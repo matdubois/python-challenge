ba918dde75731a57973108cdf902a508410e4b92
matdubois <50056480+matdubois@users.noreply.github.com>
2019-05-11 00:58:10 -0400

    Add files via upload

---
+++ PyPoll/PyPollmain.py
@@ -0,0 +1,8 @@
+# Hashtag is the begining of the comments
+import os
+import csv  
+#This is the PyPoll code
+# Path to collect data from the Resources folder
+
+employee_inputfile = os.path.join('..', 'Resources', 'employee_data.csv')
+    
